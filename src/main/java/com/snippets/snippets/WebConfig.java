cfb7f64ed72d058b8cd1c000b68c31953e1ff75d
Georgios Drivas <78447254+GeorgiosDrivas@users.noreply.github.com>
2024-09-15 22:10:18 +0300

    Add render url for CORS

--- src/main/java/com/snippets/snippets/WebConfig.java
+++ src/main/java/com/snippets/snippets/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://codekeep.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // If you're using cookies or authentication headers
